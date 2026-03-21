7a69de18f3bf86eb9e37139aeb83fd175de3ab5e
Ryan Souza <ryanssebadelhe@gmail.com>
2026-03-21 09:27:38 -0300

    feat: implement password recovery token email sending

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/PasswordRecover.java
@@ -0,0 +1,68 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_password_recover")
+public class PasswordRecover {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Instant expiration;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Instant expiration) {
+        this.expiration = expiration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PasswordRecover that = (PasswordRecover) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
--- backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -1,13 +1,56 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.entities.PasswordRecover;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.PasswordRecoverRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
+    @Value("${email.password-recover.token.minutes}")
+    private Long tokenMinutes;
+
+    @Value("${email.password-recover.uri}")
+    private String recoverUri;
+
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordRecoverRepository passwordRecoverRepository;
+
+    @Transactional
     public void createRecoverToken(EmailDTO body) {
 
+        User user = userRepository.findByEmail(body.getEmail());
+        if(user == null){
+            throw new ResourceNotFoundException("Email não encontrado");
+        }
 
+        String token = UUID.randomUUID().toString();
+
+        PasswordRecover entity = new PasswordRecover();
+        entity.setEmail(body.getEmail());
+        entity.setToken(token);
+        entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60));
+        entity = passwordRecoverRepository.save(entity);
+
+        String text = "Acesse o link para redefinir sua senha\n\n"
+                + recoverUri + token + "Validade de "+ tokenMinutes + "minutos";
+
+        emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
     }
 }
